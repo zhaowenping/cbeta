8879e4f0b4fe83f72eeb4fccb228fb52a841ac6f
zhaowp <wenping_zhao@126.com>
2017-10-21 09:22:43 +0800

    更新说明

---
+++ dict/x1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Language Version: 2.7+
+# Last Modified: 2017-10-21 09:21:41
+from __future__ import unicode_literals, division, absolute_import, print_function
+
+"""
+
+"""
+
+__all__ = []
+__author__ = "北京秘银科技 赵文平(email:wenping_zhao@126.com tel:13511028685)"
+__version__ = "0.0.1"
+
+import gzip
+import json
+
+with open('庄春江汉译阿含经词典ver4.json') as fd:
+    dd = json.load(fd)
+
+print(len(dd))
+
+def main():
+    ''''''
+
+def test():
+    ''''''
+
+if __name__ == "__main__":
+    # main()
+    test()
+
