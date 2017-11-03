1d016d108a208b7f177c77f97aa87da767621b18
zhaowp <wenping_zhao@126.com>
2017-11-03 20:45:07 +0800

    在可雅的帮助下完善排版, 默认32K本

--- fo/mk_pdf.sh
+++ fo/mk_pdf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 使用说明: 修改fop.xconf中字体文件所在位置(directory标签内容).然后执行
+# $ sh mk_pdf.sh tei_format_xml_file_name
+# 即可在当前目录生成同名pdf文件. 使用fop版本2.1
+
 # fop -c fop.xconf -xml ../../xml/T01/T01n0001_001.xml  -xsl tei2fo.xsl -pdf name.pdf
 filename=${1##*/}
 fop -c fop.xconf -xml $1 -xsl tei2fo.xsl -pdf "${filename%.*}.pdf"
