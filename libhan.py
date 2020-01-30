5ed7ada13c8cbecdd90e24893aeb56ac4d7ca2b7
zhaowp <wenping_zhao@126.com>
2020-01-29 17:34:39 -0800

    更新

--- libhan.py
+++ libhan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Language Version: 2.7+
-# Last Modified: 2020-01-29 17:30:03
+# Last Modified: 2020-01-29 17:34:35
 from __future__ import unicode_literals, division, absolute_import, print_function
 
 """
@@ -112,7 +112,7 @@ def ishanzi(zi):
 def readdb(path, trans=False, reverse=False):
     '''读取文本数据库, trans为是否用于tanslate函数, reverse为是否翻转'''
     result = dict()
-    path = os.path.join("/home/zhaowp/cbeta/cbeta", path)
+    # path = os.path.join("/home/zhaowp/cbeta/cbeta", path)
     with open(path, encoding='utf8') as fd:
         for line in fd:
             line = line.strip()
