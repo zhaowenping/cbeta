fd4516eaf2c6ec91eb731b15200274c4c176c568
zhaowp <wenping_zhao@126.com>
2020-02-28 06:13:01 -0800

    更新

---
+++ toc.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Language Version: 2.7+
+# Last Modified: 2020-02-28 06:12:00
+from __future__ import unicode_literals, division, absolute_import, print_function
+
+"""
+
+"""
+
+__all__ = []
+__author__ = "北京秘银科技 赵文平(email:wenping_zhao@126.com tel:13511028685)"
+__version__ = "0.0.1"
+
+import re
+
+p = re.compile(r'<tree text="(.*?)" action="cscd/(.*?).xml"')
+
+t = 'vin04t.nrf.toc'
+with open(f'../pali/toc3/{t}.xml') as fd:
+    for line in fd:
+        line = line.strip()
+        if not line: continue
+        if 'encoding' in line: continue
+        r = p.findall(line)
+        if not r:
+            print(line)
+        else:
+            print(' '.join((r[0][1], r[0][0])))
+
+def main():
+    ''''''
+
+def test():
+    ''''''
+
+if __name__ == "__main__":
+    # main()
+    test()
+
