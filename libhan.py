099557fc4ad0fd3ad9812b6cbeeb7080ec60d33f
zhaowp <wenping_zhao@126.com>
2020-05-22 08:30:20 -0700

    更新

--- libhan.py
+++ libhan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Language Version: 2.7+
-# Last Modified: 2020-05-21 18:45:27
+# Last Modified: 2020-05-22 08:30:14
 from __future__ import unicode_literals, division, absolute_import, print_function
 
 """
@@ -56,6 +56,8 @@ def rm_com(ctx):
     for com in re.findall(r'\[.*?\]', ctx):
         if com in desc:
             ctx = ctx.replace(com, desc[com])
+        else:
+            ctx = ctx.replace(com, ' ')
     return ctx
 
 
@@ -132,7 +134,7 @@ def ishanzi(zi):
 def readdb(path, trans=False, reverse=False):
     '''读取文本数据库, trans为是否用于tanslate函数, reverse为是否翻转'''
     result = dict()
-    #path = os.path.join("/home/zhaowp/cbeta/cbeta", path)
+    # path = os.path.join("/home/zhaowp/cbeta/cbeta", path)
     with open(path, encoding='utf8') as fd:
         for line in fd:
             line = line.strip()
--- xx.py
+++ xx.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Language Version: 2.7+
-# Last Modified: 2020-05-21 19:52:24
+# Last Modified: 2020-05-22 03:46:17
 from __future__ import unicode_literals, division, absolute_import, print_function
 
 """
@@ -15,14 +15,23 @@ __version__ = "0.0.1"
 import re
 import json
 
+with open('dict/desc.json') as fd:
+    desc = json.load(fd)
+
 def rm_com(ctx):
     # 删除组字式
-    with open('dict/desc.json') as fd:
-        desc = json.load(fd)
     # pattern = '|'.join(['\[%s\]' % k[1:-1].replace('*', '\*').replace('?', '\?')  for k in desc.keys()])
     for com in re.findall(r'\[.*?\]', ctx):
         if com in desc:
             ctx = ctx.replace(com, desc[com])
+    return ctx
+
+def rm_com2(ctx):
+    # 删除组字式
+    # pattern = '|'.join(['\[%s\]' % k[1:-1].replace('*', '\*').replace('?', '\?')  for k in desc.keys()])
+    com1 = [desc.get(cc, ' ') for cc in re.findall(r'\[.*?\]', ctx)]
+    com2 = re.split(r'\[.*?\]', ctx)
+
     return ctx
 
 print(rm_com('爾推之𨿽𡮢變[厂@已]𣅜未[幾-(戈@人)+(弋@ㄅ)]而復正'))
