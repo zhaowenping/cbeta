ad2645699246a33f70fd73672f80168de5653fec
zhaowp <wenping_zhao@126.com>
2020-08-15 20:04:52 -0700

    更新

--- libhan.py
+++ libhan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Language Version: 2.7+
-# Last Modified: 2020-08-13 09:06:41
+# Last Modified: 2020-08-15 20:04:48
 from __future__ import unicode_literals, division, absolute_import, print_function
 
 """
@@ -214,7 +214,7 @@ def unicode_zone(char):
 def readdb(path, trans=False, reverse=False):
     '''读取文本数据库, trans为是否用于tanslate函数, reverse为是否翻转'''
     result = dict()
-    # path = os.path.join("/home/zhaowp/cbeta/cbeta", path)
+    #path = os.path.join("/home/zhaowp/cbeta/cbeta", path)
     with open(path, encoding='utf8') as fd:
         for line in fd:
             line = line.strip()
