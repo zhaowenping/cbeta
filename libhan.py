7fea740e8b1dbd2ddc5169f1beab920a65c2e8e2
zhaowp <wenping_zhao@126.com>
2020-11-15 20:33:37 -0800

    更新

--- libhan.py
+++ libhan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # Language Version: 2.7+
-# Last Modified: 2020-11-13 05:42:55
+# Last Modified: 2020-11-15 20:33:33
 from __future__ import unicode_literals, division, absolute_import, print_function
 
 """
@@ -70,6 +70,10 @@ def VERTICAL(ctx):
           0xfe43: 0x300e,  # 左外引号
           0xfe44: 0x300f,  # 右外引号
             }
+    # ！＂＃＄％＆＇（）＊＋，－．／
+    # ：；＜＝＞？＠
+    # ［＼］＿
+    # ｛｜｝～
     ctx = ctx.translate(t)
     return ctx
 
@@ -276,7 +280,7 @@ def unicode_zone(char):
 def readdb(path, trans=False, reverse=False):
     '''读取文本数据库, trans为是否用于tanslate函数, reverse为是否翻转'''
     result = dict()
-    #path = os.path.join("/home/zhaowp/cbeta/cbeta", path)
+    # path = os.path.join("/home/zhaowp/cbeta/cbeta", path)
     with open(path, encoding='utf8') as fd:
         for line in fd:
             line = line.strip()
---
+++ variants/pp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Language Version: 2.7+
+# Last Modified: 2020-11-15 01:55:36
+from __future__ import unicode_literals, division, absolute_import, print_function
+
+"""
+
+"""
+
+__all__ = []
+__author__ = "北京秘银科技 赵文平(email:wenping_zhao@126.com tel:13511028685)"
+__version__ = "0.0.1"
+
+for i in (5,7,11,13,19,23,29,31,37):
+    for j in (5,7,11,13,19,23,29,31,37):
+        k = i*j
+        print(f'{i}*{j}={k}', end='')
+        print('\t', end='')
+    print('')
+
+def main():
+    ''''''
+
+def test():
+    ''''''
+
+if __name__ == "__main__":
+    # main()
+    test()
+
