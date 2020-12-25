6c1f3462e00fb4893ee0de9f3a3f758d797415a0
zhaowp <wenping_zhao@126.com>
2020-12-25 03:49:54 -0800

    更新

--- static/my.js
+++ static/my.js
@@ -196,6 +196,12 @@ function pinyin(evt){
 
 // 对文石墨水屏取消背景图片
 if (navigator.userAgent.indexOf("Note2_YZB")!=-1){
-   document.getElementById("body").style.background = null;
+   document.getElementById("body").style.backgroundImage = null;
+}
+
+// 对非文石墨水屏设置背景图片
+window.onload = function(){
+if (navigator.userAgent.indexOf("Note2_YZB")==-1){
+   document.getElementById("body").style.background = "#fffbf0 url(/static/bg.jpg) repeat";
 }
 
